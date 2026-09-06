fb286963aca9d3c64191fb587bbdf62f1e57d04b
Fábio Alves <fabio@mundodigitalsolucoes.com.br>
2026-09-06 13:42:46 -0300

    set pt-BR as default language for MDS Food

--- front/src/app/app.config.ts
+++ front/src/app/app.config.ts
@@ -24,7 +24,7 @@ export const appConfig: ApplicationConfig = {
     ),
     importProvidersFrom(
       TranslateModule.forRoot({
-        defaultLanguage: 'en',
+        defaultLanguage: 'pt-BR',
         loader: {
           provide: TranslateLoader,
           useFactory: HttpLoaderFactory,
