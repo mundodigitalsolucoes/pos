7517975f9f53467ec2d96850d6ce93921b3ae199
Fábio Alves <fabio@mundodigitalsolucoes.com.br>
2026-09-13 20:20:10 -0300

    feat(catalogo): conecta rotas de combos

--- back/app/tenant_subcategory_routes.py
+++ back/app/tenant_subcategory_routes.py
@@ -19,10 +19,12 @@ from .tenant_subcategories import (
 )
 from .catalog_modifier_routes import router as catalog_modifier_router
 from .catalog_modifier_checkout_routes import router as catalog_modifier_checkout_router
+from .catalog_combo_routes import router as catalog_combo_router
 
 router = APIRouter()
 router.include_router(catalog_modifier_router)
 router.include_router(catalog_modifier_checkout_router)
+router.include_router(catalog_combo_router)
 
 
 class SubcategoryCreateBody(SQLModel):
